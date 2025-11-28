470aab8f3ea653207fc203da0c7b34e4234e15c4
Mayank-Radadiya <mayankradadiya2@gmail.com>
2025-11-28 16:44:58 +0530

    08- Setup sentry for Error hendling

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -17,6 +17,11 @@ export const execute = inngest.createFunction(
       model: google("gemini-2.5-flash"),
       system: "You are a helpful assistant. ",
       prompt: "what is the future of AI?",
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
     });
 
     return { steps };
